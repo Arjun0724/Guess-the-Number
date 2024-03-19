b5ef5f2b007c70f83ef832ac71c9c86934f07ee0
Arjun <112996808+Arjun0724@users.noreply.github.com>
2024-03-19 13:00:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var num=(Math.ceil(Math.random()*100));
+var count=1;
+console.log(num);
+
+function findNumber()
+{
+    var numUser= parseInt(document.getElementById("n").value);
+    if(numUser==num){
+        document.getElementById("res").textContent="Congratulations!! You guessed the number in "+count+" tries";
+    }
+    else if(numUser>num){
+        document.getElementById("res").textContent="Try with smaller number";
+    }
+    else{
+        document.getElementById("res").textContent="Try with the greater number";
+    }
+    document.getElementById("tries").textContent= count;
+    count++;
+}
